881e419b47709e99197f4841879ac3170a1d6ed9
Pickle-boi <noahjhermann@gmail.com>
2023-09-08 15:31:34 -0500

    Updated Front Left Motor to move in correct direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTrain.java
@@ -100,7 +100,7 @@ public class MecanumTrain extends LinearOpMode {
         // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
         // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
         // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
