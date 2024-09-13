adb188b4930f0d4dce36da9babb4f36e5f929d6f
Ne-el <neel.mittal101@gmail.com>
2024-09-12 21:54:00 -0500

    Enabled Motor Verification - this is to reconfigure drivetrain config on Driver Hub

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTesting.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -11,7 +10,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name = "MotorVerification")
-@Disabled
 public class TeleOpTesting extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
     MecanumTrain bot;
