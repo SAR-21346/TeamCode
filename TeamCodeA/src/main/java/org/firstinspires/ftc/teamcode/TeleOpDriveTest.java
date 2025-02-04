3ab0aa871f8059f4c7a5bfca036fe4c2d21398e9
Ne-el <neel.mittal101@gmail.com>
2025-02-04 10:13:27 -0600

    mecanum teleop drive testing

---
+++ TeamCodeA/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDriveTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Drive Test", group = "testing")
+public class TeleOpDriveTest extends OpMode {
+    MecanumTrain bot;
+
+    @Override
+    public void init() {
+        bot = new MecanumTrain(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        double[] powers = bot.calculateMotorPowers(y,x,rx);
+        bot.setMotorPowers(powers[0], powers[1], powers[2], powers[3], 1);
+
+        telemetry.addData("leftFront", powers[0]);
+        telemetry.addData("leftRear", powers[1]);
+        telemetry.addData("rightFront", powers[2]);
+        telemetry.addData("rightRear", powers[3]);
+
+        telemetry.update();
+    }
+}
