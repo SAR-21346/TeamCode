173331f88b09b6d33c6c26c34a8f3084dcfc1538
Ne-el <neel.mittal101@gmail.com>
2025-02-07 09:20:14 -0600

    servo pos

--- TeamCodeA/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCodeA/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -9,8 +9,12 @@ public class RobotConstants {
     public static final double ROBOT_SIDE_LENGTH = 16;
 
     // Servo Positions
-    public static double LEFT_EXT_MIN = 0.65;
-    public static double LEFT_EXT_MAX = 0.95;
-    public static double RIGHT_EXT_MIN = 0.02;
-    public static double RIGHT_EXT_MAX = 0.65;
+    public static double LEFT_EXT_MIN = 0.1;
+    public static double LEFT_EXT_MAX = 0.62;
+    public static double RIGHT_EXT_MIN = 0.1;
+    public static double RIGHT_EXT_MAX = 0.62;
+    public static double LEFT_DROPDOWN_MIN = 0.15;
+    public static double LEFT_DROPDOWN_MAX = 0.5;
+    public static double RIGHT_DROPDOWN_MIN = 0.07;
+    public static double RIGHT_DROPDOWN_MAX = 0.42;
 }
