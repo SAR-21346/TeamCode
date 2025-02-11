b8aa7dee81d5030b89f6389f3d5aeedc8dca7a44
Ne-el <neel.mittal101@gmail.com>
2025-02-10 20:10:48 -0600

    intake state, servo positions (dropdown/ext), intake motor power constant

--- TeamCodeA/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCodeA/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -8,13 +8,32 @@ public class RobotConstants {
     public static final double ROBOT_BACK_LENGTH = 16.25;
     public static final double ROBOT_SIDE_LENGTH = 16;
 
+    // Intake States
+    public enum IntakeState {
+        INIT,
+        DISTANCE_CHECK_ENABLE,
+        DISTANCE_CHECK,
+        EXTEND,
+        PIVOT_DOWN,
+        PIVOT_DOWN_BYPASS,
+        INTAKE_ENABLE,
+        INTAKE_ACCEPT,
+        INTAKE_REJECT,
+        PIVOT_UP,
+        STOP
+    }
+
     // Servo Positions
     public static double LEFT_EXT_MIN = 0.1;
     public static double LEFT_EXT_MAX = 0.62;
     public static double RIGHT_EXT_MIN = 0.1;
     public static double RIGHT_EXT_MAX = 0.62;
-    public static double LEFT_DROPDOWN_MIN = 0.15;
-    public static double LEFT_DROPDOWN_MAX = 0.5;
-    public static double RIGHT_DROPDOWN_MIN = 0.07;
-    public static double RIGHT_DROPDOWN_MAX = 0.42;
+    public static double LEFT_DROPDOWN_MIN = 0.1;
+    public static double LEFT_DROPDOWN_MAX = 0.52;
+    public static double RIGHT_DROPDOWN_MIN = 0.13;
+    public static double RIGHT_DROPDOWN_MAX = 0.54;
+
+    // Intake Power
+    public static double INTAKE_POWER_POS = 0.8;
+    public static double INTAKE_POWER_NEG = -0.8;
 }
