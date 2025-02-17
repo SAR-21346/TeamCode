15387706c3871c864296ef993960dc3f74b96070
Neel Mittal <neel.mittal101@gmail.com>
2025-02-17 00:47:26 -0600

    localizer was being weird

--- TeamCodeA/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ TeamCodeA/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -5,8 +5,8 @@ import com.pedropathing.localization.constants.*;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = 0.00297952233;
-        ThreeWheelConstants.strafeTicksToInches = -0.0029259956;
+        ThreeWheelConstants.forwardTicksToInches = 0.00294567; // 0.0029468431, 0.00294177178, 0.0029454338, 0.002945901, 0.00294823653, 0.0029439639
+        ThreeWheelConstants.strafeTicksToInches = 0.002982075; // 0.002971800, 0.002980075, 0.0029765267,  0.002981959,0.0029788379, 0.002955759, 0.00298381407, 0.0029733620
         ThreeWheelConstants.turnTicksToInches = 0.003003217969;
         ThreeWheelConstants.leftY = 6.625;
         ThreeWheelConstants.rightY = -6.625;
